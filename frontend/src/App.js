4c1631871eb0d6f75d0fa22484250832532d7341
Neeraj Kumar <nk35715945@gmail.com>
2024-04-30 19:50:28 +0530

    radio button edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,14 @@ import { useState } from 'react';
 
 function App() {
    const[output,setOutput] = useState() ;
+   const[checked,setCheck] = useState("") ;
 
+  
+   
 
   const getIPaddress = ()=>{
+
+    
      
   }
 
@@ -21,10 +26,21 @@ function App() {
         <br />
         <div className="check_box">
           <div>
-            <input type="radio" name="ip" value="ip4" /> <label>IPv4 Address</label>
+            <input 
+               className='radio_check'
+               type="radio" name="ip"
+               value="ip4" 
+               onChange={()=>setCheck("ip4")}
+             /> 
+               <label>IPv4 Address</label>
           </div>
           <div>
-            <input type="radio" name="ip" value="ip6"/> <label>IPv6 Address</label>
+            <input 
+              className='radio_check' 
+              type="radio" name="ip" value="ip6"
+              onChange={()=>setCheck("ip6")}
+            />
+            <label>IPv6 Address</label>
           </div>
         </div>
         <button onClick={getIPaddress()}>Get IP Address</button>
